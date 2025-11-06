72b0439c09743c269c367cb6623117ad8ce7fa8a
Jin Minu <s9430939@naver.com>
2025-11-06 23:32:50 +0900

    printk문 추가

--- led_btn_module.c
+++ led_btn_module.c
@@ -18,9 +18,11 @@ static int gpio_irq_num = 0;
 static irqreturn_t btn_irq_handler(int irq, void *dev_id) {
     if (led_state == 0) {
         gpio_set_value(GPIO_LED, 1);
+        printk(KERN_INFO "gpio set 1\n");
     }
     else {
         gpio_set_value(GPIO_LED, 0);
+        printk(KERN_INFO "gpio set 0\n");
     }
     return IRQ_HANDLED;
 }
@@ -29,28 +31,34 @@ static int __init led_btn_init(void) {
     int ret;
 
     if (gpio_request(GPIO_BTN, DRIVER_NAME) != 0) { // gpio설정 요청
+        printk(KERN_INFO "BTN request fail\n");
         return -1;
     }
 
     if (gpio_direction_input(GPIO_BTN) != 0) { // input으로 설정
+        printk(KERN_INFO "gpio direction input\n");
         return -1;
     }
 
     gpio_irq_num = gpio_to_irq(GPIO_BTN); // gpio를 irq에 등록 요청
     if (gpio_irq_num < 0) {
+        printk(KERN_INFO "gpio irq fail\n");
         return -1;
     }
 
     ret = request_irq(gpio_irq_num, btn_irq_handler, IRQF_TRIGGER_FALLING,DRIVER_NAME ,NULL);
     if (ret < 0) {
+        printk(KERN_INFO "irq request fail\n");
         return -1;
     }
 
     if (gpio_request(GPIO_LED, DRIVER_NAME) != 0) {
+        printk(KERN_INFO "LED request fail\n");
         return -1;
     }
 
     if (gpio_direction_output(GPIO_LED, 0) != 0) {
+        printk(KERN_INFO "LED setting output 0\n");
         return -1;
     }
 
