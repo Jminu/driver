5b35cadfb0ea670bf88b550662359e683da79cb5
Jin Minu <s9430939@naver.com>
2025-11-12 00:12:22 +0900

    데이터 시트 읽고 명령어, 쓰기모드 구현

--- spi/7735_driver.c
+++ spi/7735_driver.c
@@ -13,6 +13,21 @@ static struct st7735_priv {
     struct gpio_desc *bl; // BCM 27
 };
 
+// spi 통신 함수
+// dc가 low : command byte
+// dc가 high : as a parameter
+static void st7735_write_cmd(struct st7735_priv *priv, unsigned char cmd) {
+    gpiod_set_value(priv->dc, 0); // 0으로 설정 command 모드로 설정
+    spi_write(priv->spi, &cmd, 1); // @priv->spi: 데이터를 쓸 디바이스
+                                  // @cmd: data buffer
+                                  // @1: data buffer size
+}
+
+static void st7735_write_data(struct st7735_priv *priv, const char *buf, size_t len) {
+    gpiod_set_value(priv->dc, 1); // 1로 설정 data write 모드로 설정
+    spi_write(priv->spi, buf, len); // 쓰기모드에서 buf데이터를 len크기만큼 write
+}
+
 // spi드라이버를 디바이스에 바인딩
 static int st7735_custom_probe(struct spi_device *spi) {
     struct st7735_priv *priv;
@@ -21,6 +36,7 @@ static int st7735_custom_probe(struct spi_device *spi) {
     priv = devm_kzalloc(&(spi->dev), sizeof(struct st7735_priv), GFP_KERNEL); // spi디바이스를 위한 메모리할당
     priv->spi = spi;
 
+    // gpio핀 가져와서 priv에 저장
     priv->reset = devm_gpiod_get(&(spi->dev), "reset", GPIOD_OUT_HIGH);
     priv->dc = devm_gpiod_get(&(spi->dev), "dc", GPIOD_OUT_HIGH);
     priv->bl = devm_gpiod_get(&(spi->dev), "bl", GPIOD_OUT_HIGH);
@@ -45,6 +61,15 @@ static int st7735_custom_probe(struct spi_device *spi) {
     gpiod_set_value(priv->bl, 1);
     msleep(100);
 
+    // software reset
+    st7735_write_cmd(priv, 0x1); // 0x1은 software reset command
+
+    // sleep mode 해제
+    st7735_write_cmd(priv, 0x11);
+
+    // turn on backlight
+    gpiod_set_value(priv->bl, 1);
+    printk(KERN_INFO "Backlight On\n");
 
     return 0;
 }
