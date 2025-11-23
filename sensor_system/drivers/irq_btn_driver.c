ee722210691b55e11eb3906fcf509153cb4f50e8
JinMinu <s9430939@naver.com>
2025-11-23 16:49:07 +0900

    read호출 대기상태로 만들기

--- sensor_system/drivers/irq_btn_driver.c
+++ sensor_system/drivers/irq_btn_driver.c
@@ -4,21 +4,76 @@
 #include <linux/gpio.h>
 #include <linux/irqreturn.h>
 #include <linux/interrupt.h>
+#include <linux/cdev.h>
+#include <linux/fs.h>
+#include <linux/device.h>
+#include <linux/uaccess.h>
+#include <linux/class.h>
+#include <linux/wait.h>
+#include <linux/sched.h>
 
 #define BTN 538
 #define IRQ_NAME "button irq"
+#define DEVICE_NAME "button_device"
+#define DRIVER_NAME "button_driver"
+#define CLASS_NAME "button_class"
 
 static int irq_num;
-int g_system_mode = 0;
+int g_system_mode = 0; // 0: temp, 1: humid
+static dev_t dev_num;
+static struct cdev btn_cdev;
+static struct device btn_dev;
+static struct class *class;
+
+static DECLARE_WAIT_QUEUE_HEAD(wq);
+static int flag = 0;
 
 static irqreturn_t irq_btn_handler(int irq, void *data) {
-	if (g_system_mode == 0) {
-		g_system_mode = 1;
-	}
-	else {
-		g_system_mode = 0;
-	}
+	wake_up_interruptible(&wq); // wait queue에 들어가있는 태스크 깨움
+	g_system_mode = 1; // 버튼 눌림 처리
 	return IRQ_HANDLED;
+}
+
+static ssize_t read_btn(struct file *file, char __user *buf, size_t len, ) {
+	wait_event_interruptible(wq, flag != 0); // wait queue로 들어감
+	flag = 0;
+	char msg = '1';
+	int ret;
+
+	ret = copy_to_user(buf, &msg, 1); // 문자 1을 유저 단으로 보냄
+	if (ret < 0) {
+		printk(KERN_ERR "copy to user fail\n");
+		return -1;
+	}
+
+	return 1;
+}
+
+static const struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.read = read_btn,
+};
+
+static void create_cdev() {
+	int ret;
+	
+	ret = alloc_chrdev_region(&dev_num, 0, 1, DEVICE_NAME);
+	if (ret != 0) {
+		printk(KERN_ERR "alloc chrdev region err\n");
+		return -1;
+	}
+
+	cdev_init(&btn_cdev, &fops);
+	ret = cdev_add(&btn_cdev, dev_num, 1);
+	if (ret != 0) {
+		printk(KERN_ERR "cdev add fail\n");
+		return -1;
+	}
+
+	class = class_create(CLASS_NAME);
+	device_create(class, NULL, dev_num, NULL, "DEVICE_NAME");
+
+	printk(KERN_INFO "create device success\n");
 }
 
 static int __init btn_init(void) {
@@ -35,6 +90,8 @@ static int __init btn_init(void) {
 		printk(KERN_ERR "request irq fail\n");
 		return -1;
 	}
+
+	create_cdev();
 
 	printk(KERN_INFO "btn module init\n");
 	return 0;
